b37d711525e2e0e87627234347dc377b968b476c
kaizen403 <rishivhavle21@gmail.com>
2023-06-01 00:07:31 +0530

    style changes

--- blog_reg.php
+++ blog_reg.php
@@ -2,16 +2,18 @@
     <section class="login-form-wrap">
         <h1>Login</h1>
         <form class="login-form" name="signup-form" action='reg.php' method='POST' >
-          <label class="eml">
+          <div>
             <input type="text" name="username" placeholder="Username" required>
-          </label>
-          <label class="eml">
+          </div>
+          <div>
             <input type="password" name="password" placeholder="Password" required>
-          </label>
-          <label class="eml">
-          <input type="email" name="email" placeholder="Email" required />
-          </label>
-          <input type="submit" name="register" value="register">
+          </div>
+          <div>
+            <input type="email" name="email" placeholder="Email" required>
+          </div>
+          <div>
+            <input type="submit" name="register" value="Register">
+          </div>
         </form>
         <h5><a href="blog_reg.php">Forgot password</a></h5>
       </section>
